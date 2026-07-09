36311f5279d7bce331257bf8e37f9a5c0bfa6810
realleotavares <realleotavares@users.noreply.github.com>
2026-07-08 21:03:54 -0300

    fix: ignora erro do dotenv caso o arquivo .env nao exista em producao

--- src/app.rs
+++ src/app.rs
@@ -32,7 +32,7 @@ impl App {
 
         tracing_subscriber::registry().with(layer).init();
 
-        dotenvy::dotenv()?;
+        dotenvy::dotenv().ok();
         let state = AppState::new().await?;
 
         let listener = TcpListener::bind("0.0.0.0:3000").await?;
